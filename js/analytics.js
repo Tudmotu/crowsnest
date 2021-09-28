a119a37ae9e0cea0d37cd7162b04ac3a1df8d414
Yotam Bar-on <tudmotu@gmail.com>
2021-09-28 19:02:20 +0300

    Separate mixpanel events from localhost to test project

--- js/analytics.js
+++ js/analytics.js
@@ -1,8 +1,13 @@
 import './mixpanel_snippet.js';
 
-mixpanel.init('8785e9f6efaf08ff28454aca7b2dee64', {
-    debug: location.hostname === 'localhost'
-}); 
+const debug = location.hostname === 'localhost';
+
+if (debug) {
+    mixpanel.init('b386f4b5dd2c5cdf5361cdfad56924cf', { debug }); 
+}
+else {
+    mixpanel.init('8785e9f6efaf08ff28454aca7b2dee64'); 
+}
 
 export function walletExists () {
     mixpanel.track('wallet_exists');
