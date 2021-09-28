008f98146f2ce0ecf55684439d480085c0b6a256
Yotam Bar-on <tudmotu@gmail.com>
2021-09-29 01:39:24 +0300

    Add collection pie chart to stats

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
-import { ethers } from "../node_modules/ethers/dist/ethers.esm.js";
+import { ethers } from '../node_modules/ethers/dist/ethers.esm.js';
+import '../node_modules/chart.js/dist/chart.js';
+import '../node_modules/chartjs-plugin-datalabels/dist/chartjs-plugin-datalabels.min.js';
 import * as opensea from './opensea.js';
 import * as analytics from './analytics.js';
 
 let provider;
+
+
+window.Chart.register(window.ChartDataLabels);
 
 window.connectWallet.addEventListener('click', async () => {
     const accounts = await window.ethereum.request({
@@ -132,4 +137,69 @@ async function init(userAddress) {
 
     window.stats.classList.remove('hidden');
     window.collectionList.classList.remove('hidden');
+
+    renderPieChart(window.statCollectionPieChart, collections);
+}
+
+function renderPieChart (el, collections) {
+    const bodyStyle = getComputedStyle(document.body);
+    const textColor = bodyStyle.getPropertyValue('--c-white');
+    const c1 = bodyStyle.getPropertyValue('--c-blue-grey');
+    const c2 = bodyStyle.getPropertyValue('--c-light');
+    const c3 = bodyStyle.getPropertyValue('--c-secondary');
+    const c4 = bodyStyle.getPropertyValue('--c-main');
+    const c5 = bodyStyle.getPropertyValue('--c-black');
+    const colors = [c1, c2, c3, c4, c5];
+
+    const collectionsOrderedByVal = collections.sort((a, b) =>{
+        const aVal = a.owned_asset_count * a.stats.floor_price;
+        const bVal = b.owned_asset_count * b.stats.floor_price;
+        return bVal - aVal;
+    });
+
+    const topFourCollections = collectionsOrderedByVal.slice(0, 4);
+    const otherCollections = collectionsOrderedByVal.slice(4);
+
+    const labels = topFourCollections.map(c => c.name);
+    labels.push('Others');
+
+    const series = topFourCollections.map(c => {
+        return c.owned_asset_count * c.stats.floor_price;
+    });
+
+    series.push(otherCollections.reduce((sum, c) => {
+        return sum + (c.owned_asset_count * c.stats.floor_price);
+    }, 0));
+
+    const chart = new Chart(window.statCollectionPieChart, {
+        type: 'pie',
+        data: {
+            labels,
+            datasets: [
+                {
+                    label: 'Collections',
+                    data: series,
+                    backgroundColor: colors
+                }
+            ]
+        },
+        plugins: [ChartDataLabels],
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+                legend: {
+                    position: 'right'
+                },
+                datalabels: {
+                    color: textColor,
+                    formatter: (value, context) => `Ξ${value.toFixed(2)}`,
+                    font: {
+                        family: '"Fira Sans"',
+                        size: 16
+                    }
+                }
+            }
+        }
+    });
 }
