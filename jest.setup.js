8d495e1b24f94922f73c4a0b5e8ee03be602d3ac
Yotam Bar-on <tudmotu@gmail.com>
2021-11-05 00:46:47 +0200

    Add mixpanel mock for tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,9 @@
 import './node_modules/@testing-library/jest-dom';
+
+jest.mock('./js/mixpanel_snippet.js', () => {
+    global.mixpanel = new Proxy({}, {
+        get () {
+            return () => {};
+        }
+    });
+});
